ec959f4d54da8d8af2bec4cb53912a83d5d66f2d
Joydip <joydip@Joydips-Mac.local>
2024-07-27 21:08:36 +0600

    project structure

---
+++ src/components/layout/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="container">
+                <p>This is a Header</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='hero'>
+        <div className="container">
+            <h1>Welcome</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
